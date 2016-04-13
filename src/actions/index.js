02c11341cb1e075da9cabbdcbf820646c42ff4bb
Triggs <triggs@webjhest.com>
2016-04-12 22:16:37 -0400

    connect searchBar to redux and bind actions creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 const API_KEY = 'c5a3964e344a2e37f4ded449adbe7dab'
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return {
+		type: 'FETCH_WEATHER',
+		payload: request
+	}
+}
